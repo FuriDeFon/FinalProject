17b8512663974ec632ec9fbdeb69efdad16936a4
FuriDeFon <A00324352@mycambrian.ca>
2025-12-04 18:30:38 -0500

    Add project files.

---
+++ FinalProject/Services/RandomPower.cs
@@ -0,0 +1,17 @@
+﻿using FinalProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Services
+{
+    public class RandomPower : IRetrieveMeasurement
+    {
+        public float RetrieveMeasurement()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinalProject/Services/RandomTemp.cs
@@ -0,0 +1,18 @@
+﻿using FinalProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Services
+{
+    public class RandomTemp : IRetrieveMeasurement
+    {
+        private Random randomNumber = new Random();
+        public float RetrieveMeasurement()
+        {
+            return randomNumber.Next(30, 50);
+        }
+    }
+}
